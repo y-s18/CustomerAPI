7ce0c8deb111234a8db8c626ff056e91fc662d0c
yamen sahyouni <yamen.sahyouni@gmail.com>
2024-03-20 16:10:30 +0100

    rename the main class

--- src/main/java/com/ys/customerApi/CustomerApiApplication.java
+++ src/main/java/com/ys/customerApi/CustomerApiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootExerciseApplication {
+public class CustomerApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootExerciseApplication.class, args);
+		SpringApplication.run(CustomerApiApplication.class, args);
 	}
 }
